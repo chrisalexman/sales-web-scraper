32eb81556ab4500b7dc9021430a73764d22bd6d6
Christopher Alexman <christopheralexman@gmail.com>
2023-02-19 15:32:51 -0800

    Add function to get toms data

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #
-# programmed by Chris, 02/16/2023
+# programmed by Chris, 02/17/2023
 #
 
 from bs4 import BeautifulSoup
@@ -7,22 +7,51 @@ import requests
 import json
 
 
-# scrape HTML from toms shoe web page, get price data
-def get_website_info():
-    toms_link = 'https://www.toms.com/us/men/shoes/sneakers/' \
-                'black-on-black-heritage-canvas-mens-carlo-sneaker-topanga-collection/10012282.html'
+def get_toms_data():
 
-    page = requests.get(toms_link)
+    link = 'https://www.toms.com/us/men/shoes/sneakers/' \
+           'black-on-black-heritage-canvas-mens-carlo-sneaker-topanga-collection/10012282.html'
+
+    page = requests.get(link)
     soup = BeautifulSoup(page.content, 'html.parser')
 
-    product_data = soup.find(id='schemaData')
-    product_dict = json.loads(product_data.text)
+    data_product = soup.find('div', {'data-product': True})['data-product']
 
-    price = product_dict['offers']['price']
+    product_dict = json.loads(data_product)['product']
 
-    print(f'price: {price}, ({type(price)})')
+    name = product_dict['brand_name'] + ' ' + product_dict['name']
 
-    # print(soup.prettify())
+    shoe = product_dict['variants'][0]
+
+    price = shoe['price']
+    size = 10
+    in_stock = shoe['in_stock']
+    image_link = shoe['image_url']
+
+    return name, price, size
+
+
+def get_moongoat_data():
+
+    link = 'https://moongoat.com/products/pocket-shop-t-shirt'
+
+    name = 'name'
+    price = 'price'
+    size = 'size'
+
+    return name, price, size
+
+
+items = [get_toms_data, get_moongoat_data]
+
+# scrape HTML from web pages, get price data and more
+def get_website_info():
+
+    for item in items:
+
+        name, price, size = item()
+
+        print(f'{name} : ${price} : size {size}')
 
 
 if __name__ == '__main__':
